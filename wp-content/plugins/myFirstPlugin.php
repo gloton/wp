d4ca53d6b7c88eeb74f66722c2856947472fd4d4
gloton <gloton@gmail.com>
2013-08-04 02:52:53 -0400

    Mi plugin 1 - Archivo base En realidad no se si sera el archivo base, pero este archivo tiene el codigo para que sea reconocido en el backend de wordpress. Sin embargo aun no tiene ninguna funcionalidad.

---
+++ wp-content/plugins/myFirstPlugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: El maravilloso pluin de Carlos
+Plugin URI: http://www.elwebmaster.cl
+Description: Este es mi primer plugin, y esto es su descripcion
+Version: 1.0
+Author: Jorge Gatica
+Author URI: http://www.w7.cl
+*/
+ ?>
