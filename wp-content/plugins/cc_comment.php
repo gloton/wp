b4b23b67aa2e20365f6c90f415c4d4b5671ffdf3
gloton <gloton@gmail.com>
2013-08-04 17:26:16 -0400

    Funciones extensibles En este sirve para extender, sobreescribiendo las funciones de wp-includes/pluggable.php, se sobre escribe en cuarquir parte de wordpress, por ejemplo en este caso sobreescribi la funcion wp_mail en el pluin creado antes por mi llamado cc_comment.php

--- wp-content/plugins/cc_comment.php
+++ wp-content/plugins/cc_comment.php
@@ -14,7 +14,7 @@ function cc_comment() {
 	$to = "j.gatica@yahoo.com";
 	$subject = "Nuevo comentario en el blog" . $_REQUEST['subject'];
 	$message = "Mensaje de; " . $_REQUEST['name'] . "por email" . $_REQUEST['email'] . "\n " . $_REQUEST['comments'];
-	mail($to, $subject, $message);
+	wp_mail($to, $subject, $message);
 }
 
 add_action("comment_post", "cc_comment")
