eb8afc97a79b1617b6d58687d124918965dbb6f5
Gloton <gloton@gmail.com>
2013-08-11 08:53:28 +0200

    Usando jQuery parte 3     Video 5.8 Parte 3         No me estaba actualizando el campo asi que vi el archivo por 	internet http://pastebin.com/WhTjVgMT, igual lo guarde en el 	evernote, solo lo tengo que buscar por el nombre del archivo 	cc_comment.php

--- wp-content/plugins/cc_comment/cc_comment.php
+++ wp-content/plugins/cc_comment/cc_comment.php
@@ -21,16 +21,18 @@ add_action("comment_post", "cc_comment");
 
 function cccomm_init() {
 	
-	register_setting('cccomm_options','cccomm_cc_email');
+	//actualiza el campo cccomm_cc_email en la pagina general
+	register_setting('general','cccomm_cc_email');
 }
 add_action('admin_init','cccomm_init');
 
 /*INICIO AGREGAR UN INPUT PARA CAMBIAR OPCIONES DE WP EN EL SUBMENU GENERAL*/
 //funcion que va mostrara el input
-function ccom_setting_field() {
+function cccom_setting_field() {
 ?>
 <input type="text" name="cccomm_cc_email" id="cccomm_cc_email"
 value="<?php echo get_option('cccomm_cc_email'); ?>" />
+<div id="emailInfo" align="left"></div>
 <?php 
 }
 function cccomm_setting_section() {
@@ -47,7 +49,7 @@ function cccomm_setting_section() {
  * */
 function cccom_plugin_menu() {
 	add_settings_section('cccomm', 'CC Comment', 'cccomm_setting_section', 'general');
-	add_settings_field('cccomm_cc_email', 'CC Comment', 'ccom_setting_field', 'general','cccomm');
+	add_settings_field('cccomm_cc_email', 'CC Comment', 'cccom_setting_field', 'general','cccomm');
 }
 /*FIN AGREGAR UN INPUT PARA CAMBIAR OPCIONES DE WP EN EL SUBMENU GENERAL*/
 
@@ -64,4 +66,12 @@ function cccomm_email_check() {
 	}
 	echo $msg;
 	die();
+}
+
+/*para incluir nuestro ajax-ojo que despues de el prefijo wp_ajax_ 
+se coloca la funcion que llamara al ajax*/
+add_action('wp_ajax_cccomm_email_check','cccomm_email_check');
+add_action('admin_print_scripts-options-general.php','cccom_email_check_script');
+function cccom_email_check_script() {
+	wp_enqueue_script("cc_comment", path_join(WP_PLUGIN_URL, basename(dirname(__FILE__))."/cc_comments.js"),array('jquery'));
 }
--- wp-content/plugins/cc_comment/cc_comments.js
+++ wp-content/plugins/cc_comment/cc_comments.js
@@ -3,16 +3,16 @@ jQuery(document).ready( function ($){
 	$("input[name='cccomm_cc_email']").blur(function(){
 		$.ajax({
 			type:"POST", 
-			data:"cccomm_cc_email="+(this)attr("value")+"&action=cccomm_email_check",
+			data:"cccomm_cc_email="+$(this).attr("value")+"&action=cccomm_email_check",
 			url:ajaxurl, //ajaxurl es una variable que wp establece por nosotros
 			beforesend:function(){//beforesend que es lo que va a pasar antes que hagamos la llamada a la funcion
 				 $("#emailInfo").html("Comprobando Correo...");
 			},
 			success:function(data){//success lo que devuelve la funcion php
 				if(data == "valid") {
-					$("#emailInfo")html("Email valido");
+					$("#emailInfo").html("Email valido");
 				} else {
-					$("#emailInfo")html("Email no vallido");
+					$("#emailInfo").html("Email no vallido");
 				}
 			}
 		});
