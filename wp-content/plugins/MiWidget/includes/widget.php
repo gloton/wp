5c6039b22bdc857b93cefe279298994ae40fbaee
jgaticaNS <jgatica@nsclick.cl>
2013-10-20 18:49:03 -0500

    El widget MiWidget ahora saca informacion al frontend gracias a la funcion widget que esta dentro del archivo /includes/widget.php

--- wp-content/plugins/MiWidget/includes/widget.php
+++ wp-content/plugins/MiWidget/includes/widget.php
@@ -8,8 +8,22 @@ class mpw_widget extends WP_Widget {
     	$this->WP_Widget('mpw_widget', "Mi primer Widget", $widget_ops);
     }
 
+    /*
+     La siguiente función es “widget”. Esta función es la que genera el 
+     contenido que se muestra en la zona del Widget, lo que verán tus 
+     usuarios en el Front End. Luego lo complicaremos, pero de momento, 
+     veamos un ejemplo simple:
+     */
     function widget($args,$instance){
         // Contenido del Widget que se mostrará en la Sidebar
+    	echo $before_widget;
+    	?>
+		<aside id='mpw_widget' class='widget mpw_widget'>
+		<h3 class='widget-title'>Mi Primer Widget</h3>
+		<p>¡Este es mi primer Widget!</p>
+		</aside>
+		<?php
+		echo $after_widget;
     }
 
     function update($new_instance, $old_instance){
