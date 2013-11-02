815fbc87f62951f37b75842d21222632013b228e
jgaticaNS <jgatica@nsclick.cl>
2013-11-02 11:34:55 -0500

    Agregando opciones y upgradeado de un plugin

--- wp-content/plugins/ch3-individual-options/ch3-individual-options.php
+++ wp-content/plugins/ch3-individual-options/ch3-individual-options.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Chapter 3 – Individual Options.
 Plugin URI:
-Description: Agregar/verificar un registro en la tabla opciones de WP
+Description: Agregar/verificar un registro en la tabla opciones de WP.Y se como hacer un upgrade del plugin
 WordPress admin interface
 Version: 1.0
 Author: Yannick Lefebvre
@@ -11,9 +11,22 @@ License: GPLv2
 */
 
 register_activation_hook( __FILE__,'ch3io_set_default_options' );
+/*antigua version
 function ch3io_set_default_options() {
 	if ( get_option( 'ch3io_ga_account_name' ) === false ) {
 		add_option( 'ch3io_ga_account_name', "UA-000000-0" );
+	}
+
+}
+*/
+function ch3io_set_default_options() {
+	if ( get_option( 'ch3io_version' ) === false ) {
+		add_option( 'ch3io_ga_account_name', 'UA-000000-0' );
+		add_option( 'ch3io_track_outgoing_links', 'false' );
+		add_option( 'ch3io_version', '1.1' );
+	} elseif ( get_option( 'ch3io_version' ) < 1.1 ) {
+		add_option( 'ch3io_track_outgoing_links', 'false' );
+		update_option( 'ch3io_version', '1.1' );
 	}
 }
 ?>
